b4b3f1afddb33af1de4540da5a3bbe4b1d70b3ae
limorc <limorc664@gmail.com>
2018-01-12 16:25:52 +0200

    added equalizer query

--- database/queries/equalizer.py
+++ database/queries/equalizer.py
@@ -1,10 +1,10 @@
 EQUALIZER = {
-    'query' : 'UPDATE user_search_history '
-              'SET is_favorite=1 '
-              'WHERE history_id=%d',
-    'params': ['history_id'],
-    'mode'  : ['select'],
-    'default':{
-        'user_name' : ''
-    }
+    'query' :   'SELECT 0.1*views + 0.2*comments + 0.3*likes + 0.2*genre + 0.2*country AS score, youtube_video_title, youtube_video_id '
+                'FROM join_song_vide_artist '
+                'WHERE genre = "%s" '
+                'AND country = "%s" '
+                'ORDER BY score '
+                'LIMIT 10',
+    'args'  : ['genre', 'country'],
+    'mode'  : ['select']
 }

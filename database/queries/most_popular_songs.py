df6678057409417a8c94ec609c8b4eb1b85d143d
Harelz <harel654@gmail.com>
2018-01-15 16:39:35 +0200

    Added readable query for example + updated documentation.

--- database/queries/most_popular_songs.py
+++ database/queries/most_popular_songs.py
@@ -1,7 +1,7 @@
 import random
 '''
 This is a query which selects top 10 most popular music videos.
-Each music video get a score. The score is as followed:
+Each music video get a score. The score is calculated as followed:
 score = video_views + 2.5*video_comments + video_likes + video_dislikes.
 We decided a popular song is a song who has a lot of comments, likes, dislikes, views and favorited a lot.
 The most important parameters for us was comments and favorites.
@@ -23,6 +23,31 @@ MOST_POPULAR_SONGS = {
                'JOIN youtube_video  as main_table ON rating_table.id = main_table.youtube_video_id '
                'ORDER BY rating  desc'
                'limit 10) as a ',
+    'args': ['release_date'],
+    'mode'  : 'select',
+    'default' : {"year":random.randint(1905,2018)}
+}
+
+HARELZ_MOST_POPULAR_SONGS = {
+    'query' :  '''
+    SELECT C.youtube_video_title, C.youtube_video_id
+    FROM
+    (
+        SELECT A.youtube_video_title, A.youtube_video_id, A.favorites,
+                A.views + 2.5 * A.comments + A.likes + A.dislikes AS score
+        FROM join_song_vide_artist AS A
+        ORDER BY A.score
+        LIMIT 10
+        UNION (
+        SELECT B.youtube_video_title, B.youtube_video_id, B.favorites,
+                0 AS score
+        FROM join_song_vide_artist AS B
+        ORDER BY B.views
+        LIMIT 5
+        )
+    ) AS C
+    ORDER BY C.favorites, C.score
+    LIMIT 10''',
     'args': ['release_date'],
     'mode'  : 'select',
     'default' : {"year":random.randint(1905,2018)}

e938805e682150c7f23a6e6116b2461b50ff2c49
Harelz <harel654@gmail.com>
2018-01-15 16:19:09 +0200

    Login query documentation.

--- database/queries/login.py
+++ database/queries/login.py
@@ -1,3 +1,9 @@
+'''
+This is a query to handle the login process.
+We select 3 attributes - login, user_id and user_name.
+login is 1 if the inserted details are correct, 0 otherwise.
+user_id and user_name are attributes in "user" table.
+'''
 USER_LOGIN = {
     'query' : 'SELECT (COUNT(*) > 0) AS login, user_id, user_name '
               'FROM ('

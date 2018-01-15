a5c61355fa07b338e13015e81caf9b8b51af53fe
Harelz <harel654@gmail.com>
2018-01-15 16:18:39 +0200

    Most popular songs query documentation.

--- database/queries/most_popular_songs.py
+++ database/queries/most_popular_songs.py
@@ -1,5 +1,15 @@
 import random
-
+'''
+This is a query which selects top 10 most popular music videos.
+Each music video get a score. The score is as followed:
+score = video_views + 2.5*video_comments + video_likes + video_dislikes.
+We decided a popular song is a song who has a lot of comments, likes, dislikes, views and favorited a lot.
+The most important parameters for us was comments and favorites.
+In the score the comments has a 2.5 weight factor to all other parameters.
+Because some uploaders can turn off comments to a video, we also select the 5 music videos with the most views.
+After we get 15 music videos (10 with highest score + 5 with most views) we select the top 10 favored music videos. 
+Meaning out of 15 music videos, final 10 music videos are the ones who people favorite the most.
+'''
 MOST_POPULAR_SONGS = {
     'query' :  'SELECT youtube_video_id, youtube_video_title '
                'FROM '

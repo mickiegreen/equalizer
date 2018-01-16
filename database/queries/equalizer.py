5e3d67fa839bf83c99ef692e65e902aba6e3b44e
kelias <karin.elias@intel.com>
2018-01-16 12:27:14 +0200

    kkakkak

--- database/queries/equalizer.py
+++ database/queries/equalizer.py
@@ -1,11 +1,11 @@
 EQUALIZER = {
     'query': '''
-                'SELECT 0.1*views + 0.2*comments + 0.3*likes + 0.2*genre + 0.2*country AS score, youtube_video_title, youtube_video_id '
-                'FROM join_song_vide_artist '
-                'WHERE genre = "%s" '
-                'AND country = "%s" '
-                'ORDER BY score '
-                'LIMIT 10''',
+                SELECT 0.1*views + 0.2*comments + 0.3*likes + 0.2*genre + 0.2*country AS score, youtube_video_title, youtube_video_id 
+                FROM join_song_vide_artist 
+                WHERE genre = "%s" 
+                AND country = "%s" 
+                ORDER BY score 
+                LIMIT 10''',
     'mode'  : 'select',
     'args': ['genre','country']
 }

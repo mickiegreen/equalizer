c8a45d851645fa9e9e3a6fdcc41686e4e3d4ccc7
Michael Green <michael.green4459@gmail.com>
2018-01-21 14:07:53 +0200

    fix to query

--- database/queries/search/get_most_hated_songs_of_year.py
+++ database/queries/search/get_most_hated_songs_of_year.py
@@ -3,20 +3,20 @@ return most hated songs in specified year
 """
 MOST_HATED_SONGS_OF_YEAR = {
     'query': '''
-                SELECT youtube_video_id AS youtube_video_id, youtube_video_title, video_id 
-                FROM (
-                  SELECT `year`, main_table.youtube_video_id, youtube_video_title, video_id
-                  FROM ( 
-                    SELECT YEAR(release_date) AS `year`, youtube_video_id,
-                      SUM(0.3*views+0.5*likes+0.2*comments) AS rating 
-                    FROM artist_song_video_view 
-                    WHERE country IN(SELECT country FROM song WHERE YEAR(release_date) = %d) 
-                    GROUP BY youtube_video_id) 
-                  AS rating_table 
-                  JOIN youtube_video AS main_table ON rating_table.youtube_video_id = main_table.youtube_video_id
-                  ORDER BY rating  
-                  LIMIT 10 
-                ) AS a
+              SELECT `year`, main_table.youtube_video_id, youtube_video_title, video_id
+              FROM ( 
+                SELECT YEAR(release_date) AS `year`, youtube_video_id,
+                  0.3*SUM(views)+0.5*SUM(dislikes)+0.2*SUM(comments) AS rating 
+                FROM artist_song_video_view 
+                WHERE country IN(
+                  SELECT DISTINCT country FROM song
+                  WHERE YEAR(release_date) = %d
+                ) 
+                GROUP BY youtube_video_id) 
+              AS rating_table 
+              JOIN youtube_video AS main_table ON rating_table.youtube_video_id = main_table.youtube_video_id
+              ORDER BY rating  
+              LIMIT 10;
             ''',
     'args': ['year'],
     'mode'  : 'select',
